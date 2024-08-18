343b7167f783741eee23bdba49fb29d8ff2c6e13
Sumit Kesarwani <sumit.polite@gmail.com>
2024-08-18 20:37:28 +0530

    wer

--- src/dp/CoinChangeProblem.java
+++ src/dp/CoinChangeProblem.java
@@ -6,11 +6,11 @@ import java.util.List;
 /** condition : We can select same coin more than one time */
 public class CoinChangeProblem {
   public static void main(String[] args) {
-    int[] coins = {1, 2, 5, 7, 3, 6, 9};
+    int[] coins = { 1, 2, 5, 7, 3, 6, 9 };
     int sum = 10;
-    //  System.out.println(coinChange(coins, sum, coins.length));
+    // System.out.println(coinChange(coins, sum, coins.length));
     // coinChangeUsingDP(coins,sum);
-    coinChangeInON(sum,coins);
+    coinChangeInON(sum, coins);
   }
 
   /**
@@ -22,15 +22,17 @@ public class CoinChangeProblem {
    * @return
    */
   private static int coinChange(int[] coins, int sum, int length) {
-    if (length == 0 || sum < 0) return 0;
-    if (sum == 0) return 1;
+    if (length == 0 || sum < 0)
+      return 0;
+    if (sum == 0)
+      return 1;
 
     return coinChange(coins, sum - coins[length - 1], length) + coinChange(coins, sum, length - 1);
   }
 
   private static void coinChangeUsingDP(int[] coins, int sum) {
-    int[][] dp = new int[sum+1][coins.length + 1];
-	  List<Integer> list = new ArrayList<>();
+    int[][] dp = new int[sum + 1][coins.length + 1];
+    List<Integer> list = new ArrayList<>();
     System.out.println(coinChangeDP(coins, sum, dp, coins.length));
   }
 
@@ -43,23 +45,23 @@ public class CoinChangeProblem {
     }
     if (sum == 0) {
       dp[sum][length] = 1;
-	  return dp[sum][length];
+      return dp[sum][length];
     }
-    dp[sum][length] =
-        coinChangeDP(coin, sum - coin[length - 1], dp, length)
-            + coinChangeDP(coin, sum, dp, length - 1);
+    dp[sum][length] = coinChangeDP(coin, sum - coin[length - 1], dp, length)
+        + coinChangeDP(coin, sum, dp, length - 1);
     return dp[sum][length];
   }
 
-  private static void coinChangeInON(int sum,int[] coin){
-    int[] dp = new int[sum+1];
+  private static void coinChangeInON(int sum, int[] coin) {
+    int[] dp = new int[sum + 1];
     dp[0] = 1;
-    for(int i=0;i<coin.length;i++){
-      for (int j = coin[i];j<=sum;j++){
+    for (int i = 0; i < coin.length; i++) {
+      for (int j = coin[i]; j <= sum; j++) {
         /**
-         * dp[j-coin[i]] : this will tell us we are picking the coin and , how may time we are picking , this will tell us by inner loop
+         * dp[j-coin[i]] : this will tell us we are picking the coin and , how may time
+         * we are picking , this will tell us by inner loop
          */
-        dp[j] = dp[j]+dp[j-coin[i]];
+        dp[j] = dp[j] + dp[j - coin[i]];
       }
     }
     System.out.println(dp[sum]);
--- src/dp/ZeroOneKnapsack.java
+++ src/dp/ZeroOneKnapsack.java
@@ -5,74 +5,91 @@ package dp;
  */
 public class ZeroOneKnapsack {
     public static void main(String[] args) {
-        int[] p = {40, 100, 50, 60};
-        int[] w = {20, 10, 40, 30};
+        int[] p = { 60, 100, 120 };
+        int[] w = { 10,20,30 };
         int len = 4;
-        int weight = 60;
-        //System.out.println(getMaxProfit(w, p, len, weight));
+        int weight = 50;
+        // System.out.println(getMaxProfit(w, p, len, weight));
 
-        System.out.println(getMaxProfitDP(w, p, weight));
+            System.out.println(getMaxProfit1D(p, w, weight));
+        //System.out.println(getMaxProfit2DP(w, p, weight));
     }
 
-    public static int getMaxProfit(int[] w,int[] p,int len,int weight){
-        if(len <= 0 || weight == 0){
+    public static int getMaxProfit(int[] w, int[] p, int len, int weight) {
+        if (len <= 0 || weight == 0) {
             return 0;
         }
 
-        if(weight < w[len-1]){
-            return getMaxProfit(w,p,len-1,weight);
+        if (weight < w[len - 1]) {
+            return getMaxProfit(w, p, len - 1, weight);
         }
 
-        return Math.max(getMaxProfit(w, p, len-1, weight), p[len-1]+getMaxProfit(w, p, len-1, weight-w[len-1]));
+        return Math.max(getMaxProfit(w, p, len - 1, weight),
+                p[len - 1] + getMaxProfit(w, p, len - 1, weight - w[len - 1]));
     }
 
-    public static int getMaxProfitDP(int[] weight,int[] profit,int w){
-        int[][] arr = new int[weight.length+1][w+1];
+    public static int getMaxProfit2DP(int[] weight, int[] profit, int w) {
+        int[][] arr = new int[weight.length + 1][w + 1];
 
-        for(int i=0;i<=weight.length;i++){
+        for (int i = 0; i <= weight.length; i++) {
             arr[i][0] = 0;
         }
 
-        for(int j=0;j<=w;j++){
+        for (int j = 0; j <= w; j++) {
             arr[0][j] = 0;
         }
 
-        for(int i=1;i<=weight.length;i++){
-            for(int j=1;j<=w;j++){
-                if(j >= weight[i-1]){
-                    arr[i][j] = Math.max(arr[i-1][j], profit[i-1]+arr[i-1][j-weight[i-1]]);
-                }else{
-                    arr[i][j] = arr[i-1][j];
+        for (int i = 1; i <= weight.length; i++) {
+            for (int j = 1; j <= w; j++) {
+                if (j >= weight[i - 1]) {
+                    arr[i][j] = Math.max(arr[i - 1][j], profit[i - 1] + arr[i - 1][j - weight[i - 1]]);
+                } else {
+                    arr[i][j] = arr[i - 1][j];
                 }
             }
         }
 
         // for(int i=0;i<=weight.length;i++){
-        //     for(int j=0;j<=w;j++){
-        //         System.out.print(arr[i][j]+",");
-        //     }
-        //     System.out.println();
+        // for(int j=0;j<=w;j++){
+        // System.out.print(arr[i][j]+",");
+        // }
+        // System.out.println();
         // }
         printItem(arr, weight, w);
         return arr[weight.length][w];
     }
 
-    private static void printItem(int[][] arr,int[] weight,int bagCapacity){
+    private static void printItem(int[][] arr, int[] weight, int bagCapacity) {
 
         int res = arr[weight.length][bagCapacity];
         int ind = weight.length;
         int capacity = bagCapacity;
-        while(ind >= 1){
+        while (ind >= 1) {
 
-            if(capacity >= weight[ind-1]){
-                System.out.println(weight[ind-1]);
-                res = res - arr[ind-1][capacity - weight[ind-1]];
+            if (capacity >= weight[ind - 1]) {
+                System.out.println(weight[ind - 1]);
+                res = res - arr[ind - 1][capacity - weight[ind - 1]];
                 capacity = capacity - weight[ind - 1];
             }
             ind--;
         }
+    }
 
+    // todo: check this again
+    private static int getMaxProfit1D(int[] p,int[] w,int weight){
+        int[] dw = new int[weight+1];
+        dw[0] = 0;
 
+        for(int i=1;i<=w.length;i++){
+            for(int j =1;j<= weight;j++){
+                if(j >= w[i-1]){
+                    dw[j] = Math.max(dw[j], p[i-1]+dw[j - w[i-1]]);
+                }
+            }
+        }
+
+        System.out.println(dw[weight]);
+        return dw[weight];
     }
 
 }
