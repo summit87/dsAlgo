c1bc63b7c07b328af7dd86365afd390d1ff53326
Sumit Kesarwani <sumit.polite@gmail.com>
2024-11-23 13:30:07 +0530

    wer

--- src/stringP/LongestPalindromicSubstring.java
+++ src/stringP/LongestPalindromicSubstring.java
@@ -2,8 +2,18 @@ package stringP;
 
 public class LongestPalindromicSubstring {
     public static void main(String[] args) {
-       System.out.println(longestPalindromic("forgeeksskeegfor"));
+      // System.out.println(longestPalindromic("forgeeksskeegfor"));
+       longestPalindromicSubstring("forgeeksskeegfor");
     }
+
+    /**
+     * Below approach will take O(N^3) time complexity
+     * i.e O(N^2)* (O(N)+O(N))
+     * 
+     * since substring will also take the O(N) time complexity
+     * @param str
+     * @return
+     */
 
     private  static  String longestPalindromic(String str){
         int maxLen = Integer.MIN_VALUE;
@@ -33,4 +43,43 @@ public class LongestPalindromicSubstring {
         }
         return str.charAt(st) == str.charAt(end) && isPalindrom(str, st +1, end-1);
     }
+
+    /**
+     * The below approach will take O(N)*(O(N)+O(N)) which is ~= O(N^2)
+     */
+
+
+     private static void longestPalindromicSubstring(String str){
+        int maxLen = Integer.MIN_VALUE;
+        int start = 0;
+        for(int i=0;i<str.length();i++){
+            int st = i;
+            int end = i;
+            int len = 0;
+            while( st>= 0 && end < str.length() && str.charAt(st) == str.charAt(end)){
+                len = end-st+1;
+                if(maxLen < len){
+                    start = st;
+                    maxLen = len;
+                }
+                st--;end++;
+            }
+
+            st = i;
+            end = i+1;
+
+            while(st >=0 && end < str.length() && str.charAt(st) == str.charAt(end)){
+                len = end-st+1;
+                if(len > maxLen){
+                    maxLen = len;
+                    start = st;
+                }
+                st--;end++;
+            }
+        }
+
+        System.out.println(str.substring(start , start+maxLen));
+
+     }
+
 }
